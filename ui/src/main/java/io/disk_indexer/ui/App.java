893622fb34ad2768d734a1431b12c3dfe143a8fd
Elian Doran <elinnum@gmail.com>
2018-05-27 15:43:17 +0300

    Change Java release version for core and ui projects

--- ui/src/main/java/io/disk_indexer/ui/App.java
+++ ui/src/main/java/io/disk_indexer/ui/App.java
@@ -1,13 +1,14 @@
 package io.disk_indexer.ui;
 
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+	}
 }
