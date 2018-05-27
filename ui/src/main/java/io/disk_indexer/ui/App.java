29cea316c68e7305ca2a58e7f1f7751a75ab901a
Elian Doran <elinnum@gmail.com>
2018-05-27 15:19:23 +0300

    Create UI Maven project

---
+++ ui/src/main/java/io/disk_indexer/ui/App.java
@@ -0,0 +1,13 @@
+package io.disk_indexer.ui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
