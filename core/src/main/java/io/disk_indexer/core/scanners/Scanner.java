d03cab4033dba305ed9d1b06590c55c304196301
Elian Doran <elinnum@gmail.com>
2018-05-27 13:16:59 +0300

    Add small documentation to Scanner

--- core/src/main/java/io/disk_indexer/core/scanners/Scanner.java
+++ core/src/main/java/io/disk_indexer/core/scanners/Scanner.java
@@ -9,10 +9,21 @@ import io.disk_indexer.core.exceptions.StreamListenerFailedException;
 import io.disk_indexer.core.model.Collection;
 import io.disk_indexer.core.model.Entry;
 
+/**
+ * A scanner is the central part of the indexer. Given a path of some sort on {@link #scan(Collection, String)}, it will recursively iterate through all the files available at the given path.
+ *
+ * <p>
+ * For each file found, the {@link Scanner} will call all the registered {@link EntryListener}(s) and {@link StreamListener}(s) to attach additional functionality such as persistence to database,
+ * metadata processing, etc. These can be manually added via {@link #addEntryListener(EntryListener)} and {@link #addStreamListener(StreamListener)}. With no listeners attached, the scanner will
+ * simply iterate over all the files without any further action.
+ */
 public abstract class Scanner {
 	private List<EntryListener> entryListeners;
 	private List<StreamListener> streamListeners;
 
+	/**
+	 * Creates a new scanner.
+	 */
 	public Scanner() {
 		this.entryListeners = new LinkedList<>();
 		this.streamListeners = new LinkedList<>();
