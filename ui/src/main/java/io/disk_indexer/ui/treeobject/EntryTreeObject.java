af3e26ee225a942721257b287539203e125c4561
Elian Doran <elinnum@gmail.com>
2018-06-16 01:40:38 +0300

    Display friendly modification date

--- ui/src/main/java/io/disk_indexer/ui/treeobject/EntryTreeObject.java
+++ ui/src/main/java/io/disk_indexer/ui/treeobject/EntryTreeObject.java
@@ -1,5 +1,11 @@
 package io.disk_indexer.ui.treeobject;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
 import io.disk_indexer.core.entity.Entry;
@@ -16,7 +22,7 @@ public class EntryTreeObject extends RecursiveTreeObject<EntryTreeObject> {
 	public EntryTreeObject(Entry entry) {
 		this.name = new SimpleStringProperty(entry.getName());
 		this.size = new SimpleLongProperty(entry.getSize());
-		this.date = new SimpleStringProperty(String.valueOf(entry.getModificationDate()));
+		this.date = new SimpleStringProperty(parseDate(entry.getModificationDate()));
 	}
 
 	public StringProperty nameProperty() {
@@ -29,5 +35,12 @@ public class EntryTreeObject extends RecursiveTreeObject<EntryTreeObject> {
 
 	public StringProperty dateProperty() {
 		return this.date;
+	}
+
+	private String parseDate(long date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.MEDIUM);
+		Instant instant = Instant.ofEpochMilli(date);
+		LocalDateTime dateTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+		return dateTime.format(formatter);
 	}
 }
