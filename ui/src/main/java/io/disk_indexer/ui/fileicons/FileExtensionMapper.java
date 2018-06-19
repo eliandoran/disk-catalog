c24564b64ea19ff34a6f71ecbdb443a48136b07f
Elian Doran <elinnum@gmail.com>
2018-06-19 04:07:38 +0300

    Add directory icon

--- ui/src/main/java/io/disk_indexer/ui/fileicons/FileExtensionMapper.java
+++ ui/src/main/java/io/disk_indexer/ui/fileicons/FileExtensionMapper.java
@@ -2,12 +2,24 @@ package io.disk_indexer.ui.fileicons;
 
 
 import io.disk_indexer.core.entity.Entry;
+import io.disk_indexer.core.entity.EntryTypes;
 import javafx.scene.image.Image;
 
 public class FileExtensionMapper implements FileMapper {
 
 	@Override
 	public Image obtainIcon(Entry entry) {
-		return new Image("io/disk_indexer/ui/fileicons/file.png");
+		return loadImage(getImagePath(entry));
+	}
+
+	private static String getImagePath(Entry entry) {
+		if (entry.getEntryType() == EntryTypes.Directory)
+			return "directory.png";
+
+		return "file.png";
+	}
+
+	private static Image loadImage(String imagePath) {
+		return new Image("io/disk_indexer/ui/fileicons/" + imagePath);
 	}
 }
